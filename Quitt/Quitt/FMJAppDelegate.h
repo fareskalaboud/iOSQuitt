0941acdbbbd6748bd7e0cb55b8762573761cb58b
James Bellamy <k1209605@kcl.ac.uk>
2014-02-22 14:58:43 +0000

    init

---
+++ Quitt/Quitt/FMJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMJAppDelegate.h
+//  Quitt
+//
+//  Created by James Bellamy on 22/02/2014.
+//  Copyright (c) 2014 DragonLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quitt/Quitt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quitt
+//
+//  Created by James Bellamy on 22/02/2014.
+//  Copyright (c) 2014 DragonLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMJAppDelegate class]));
+    }
+}
